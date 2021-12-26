aa001c62bd669b47d62c7d5be0dc841f1bf5426c
Ashutosh Pradhan <ashutoshpradhan164@gmail.com>
2021-12-26 21:08:01 +0530

    react tutorial

--- src/App.js
+++ src/App.js
@@ -4,21 +4,34 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Info></Info>
+
+      <AddItem></AddItem>
+      <AddItem></AddItem>
+      <AddItem></AddItem>
+
     </div>
+  );
+}
+
+function Info(){
+  return (
+    <div>
+      <h1>Inventory System</h1>
+      <p>Manage your stuff</p>
+    </div>
+  );
+}
+
+function AddItem(){
+  return(
+ 
+      <form>
+        <label for='text-form'>Type something</label>
+        <input type='text' id='text-form'/>
+      </form>
+   
   );
 }
 
