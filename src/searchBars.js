cbbaecef7ec55e62335742a451e8335416d4c61d
Ashutosh Pradhan <ashutoshpradhan164@gmail.com>
2021-12-27 13:21:25 +0530

    input data show in page

---
+++ src/searchBars.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+
+export default function SearchBar(props) {
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState(0);
+    const [type, setType] = useState('');
+    const [brand, setBrand] = useState('');
+
+    const searchButtonPress = () => {
+        props.callback({name:name, price:price,type:type, brand:brand})
+    }
+
+
+    return (
+        <div>
+            <h2>Search for an Item</h2>
+            <form>
+                <label htmlFor='name_field'>Name: </label>
+                <input type='text' id="name_field" value={name} onChange={(e) => setName(e.target.value)}></input>
+                <label htmlFor='price-field'>Price: </label>
+                <input type='number' id="price-field" value={price} onChange={(e) => setPrice(e.target.value)}></input>
+                <label htmlFor='type-field'>Type: </label>
+                <input type='text' id="type-field" value={type} onChange={(e) => setType(e.target.value)}></input>
+                <label htmlFor='brand-field'>Brand: </label>
+                <input type='text' id="brand-field" value={brand} onChange={(e) => setBrand(e.target.value)}></input>
+                <button type="button" onClick={searchButtonPress}>Search</button>
+            </form>
+
+        </div>
+    );
+}
+// export default searchBar;
